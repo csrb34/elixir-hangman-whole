9bc5feeb8c599484a0afb204b720d941b7724f16
Carla San Roman <carla.s@thoughtworks.com>
2022-06-20 11:45:02 +0200

    Add dictionary application

---
+++ dictionary/lib/lists.ex
@@ -0,0 +1,24 @@
+defmodule Lists do
+  def len([]), do: 0
+  def len([_h|t]), do: 1 + len(t)
+
+  def sum([]), do: 0
+  def sum([ h|t ]), do: h + sum(t)
+
+  def square([]), do: []
+  def square([h|t]), do: [h*h | square(t)]
+
+  def double([]), do: []
+  def double([h|t]), do: [2*h | double(t)]
+
+  def map([], _func), do: []
+  def map([h|t], func), do: [ func.(h) | map(t, func) ]
+
+  def sum_pairs([]), do: []
+  def sum_pairs([h1, h2 | t]), do: [ h1+h2 | sum_pairs(t)]
+
+  def even_length?([]), do: false
+  def even_length?([_a]), do: false
+  def even_length?([_a, _b]), do: true
+  def even_length?([_a, _b | t]), do: even_length?(t)
+end
