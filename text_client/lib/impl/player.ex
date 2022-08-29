6a9e16669433fde7c50a024642ac691414517591
Carla San Roman <carla.s@thoughtworks.com>
2022-08-29 18:19:39 +0200

    feat: getting a guess and making a move

--- text_client/lib/impl/player.ex
+++ text_client/lib/impl/player.ex
@@ -8,6 +8,8 @@ defmodule TextClient.Impl.Player do
   def start() do
     game = Hangman.new_game()
     tally = Hangman.tally(game)
+    IO.puts game.letters |> to_string()
+    IO.puts tally.letters |> to_string()
     interact({ game, tally })
   end
 
@@ -17,8 +19,8 @@ defmodule TextClient.Impl.Player do
     IO.puts "Congratulations. You won!"
   end
 
-  def interact({ _game, tally = %{ game_state: :lost }}) do
-    IO.puts "Sorry, you lost... the word was #{tally.letters |> Enum.join}"
+  def interact({ game, _tally = %{ game_state: :lost }}) do
+    IO.puts "Sorry, you lost... the word was #{game.letters |> Enum.join}"
   end
 
   def interact({game, tally}) do
@@ -27,7 +29,11 @@ defmodule TextClient.Impl.Player do
     # get next guess
     # make move
     IO.puts feedback_for(tally)
-    #interact()
+    IO.puts current_word(tally)
+    guess = get_guess()
+    IO.puts guess
+    {update_game, update_tally} = Hangman.make_move(game, guess)
+    interact({ update_game, update_tally })
   end
 
   # @type   state :: :initializing | :good_guess | :bad_guess | :already_used | :invalid_char
@@ -36,8 +42,25 @@ defmodule TextClient.Impl.Player do
     "Welcome! I'm thinking of a #{tally.letters |> length} letter word"
   end
 
-  def feedback_for(tally = %{ game_state: :good_guess }), do: "Good guess!"
-  def feedback_for(tally = %{ game_state: :bad_guess }), do: "Sorry, that letter's not in the word"
-  def feedback_for(tally = %{ game_state: :already_used }), do: "You already used that letter"
-  def feedback_for(tally = %{ game_state: :invalid_char }), do: "Sorry, that letter is invalid no accent written or especial charecters are allowed"
+  def feedback_for(_tally = %{ game_state: :good_guess }), do: "Good guess!"
+  def feedback_for(_tally = %{ game_state: :bad_guess }), do: "Sorry, that letter's not in the word"
+  def feedback_for(_tally = %{ game_state: :already_used }), do: "You already used that letter"
+  def feedback_for(_tally = %{ game_state: :invalid_char }), do: "Sorry, that letter is invalid no accent written or especial charecters are allowed"
+
+  def current_word(tally) do
+    [
+        "Word so far: ", tally.letters |> Enum.join(" "),
+      "   turns left: ", tally.turns_left |> to_string,
+      "   used so far: ", tally.used |> Enum.join(",")
+    ]
+  end
+
+  def get_guess() do
+    IO.gets("Next letter: ")
+    |> String.trim()
+    |> String.downcase()
+
+
+
+  end
 end
